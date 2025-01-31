fb75ae8923b80d88f7b1fa4fdb4099140b57c0b0
FabricioPython <fabricio.mh@outlook.com>
2025-01-30 23:27:40 -0300

    adds

--- app.js
+++ app.js
@@ -31,7 +31,8 @@ let tentativas = 1;
 function exibirTextoNaTela(tag, texto) {
   let campo = document.querySelector(tag);
   campo.innerHTML = texto;
-  responsiveVoice.speak(texto, "Brazilian Portuguese Female", { rate: 1.2 });
+  if (tag == "h1" || tag == "p")
+    responsiveVoice.speak(texto, "Brazilian Portuguese Female", { rate: 1.2 });
 }
 
 function exibirMensagemInicial() {
